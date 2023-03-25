ad83d9fe81f0df945a1f87f5271b9a3fbb9c1c35
cardotcl <clement.cardot@reseau.eseo.fr>
2023-03-25 16:57:06 +0100

    add more info when running the build script

--- build.sh
+++ build.sh
@@ -1,16 +1,23 @@
+# This Script is used to build the project and run it in a docker container.
+
 # 1. Build Back-end
+echo "Step 1 --> Build Back-end"
 cd back
 mvn clean package
 
 # 2. Build Front-end
+echo "Step 2 --> Build Front-end"
 cd ../front/
+npm install
 ng build
 
 # 3. Build Docker Image
+echo "Step 3 --> Build Docker Image"
 cd ../
 docker build -t taf .
 
 # 4. Run Docker Image
+echo "Step 4 --> Run Docker Image"
 docker rm -f taf
 docker run --name taf -it -p 8080:8080 taf
 
