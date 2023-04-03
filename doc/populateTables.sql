fe78d1f9036e052b42904565277674f376879975
cardotcl <clement.cardot@reseau.eseo.fr>
2023-04-03 21:46:33 +0200

    remove enabled field

--- doc/populateTables.sql
+++ doc/populateTables.sql
@@ -1,10 +1,10 @@
-INSERT INTO user (name, surname, login, password, email, role, enabled) VALUES
-('Jean', 'Dupont', 'jdupont', '$2a$12$beDKCRFS7AkSAzqfuVAgjemzWSbtYRMmGmg6lMmSqymZet9egfL7q', 'jean.dupont@reseau.eseo.fr', 'student', 1),
-('Pierre', 'Durand', 'pdurand', '$2a$12$zeVlwY3Eo6mBVM3INdB4GeXWrk8PpG4/gLs3W7.C9ZLmaLVFx9SOC', 'pierre.durand@reseau.eseo.fr', 'student', 1),
-('Paul', 'Martin', 'pmartin', '$2a$12$lHpX7qcs5rf4wH4CejKBKu5j3FXqPwhyHl4Iki4vd3eSYEOfC7rbm', 'paul.martin@reseau.eseo.fr', 'student', 1),
-('Jacques', 'Petit', 'jpetit', '$2a$12$L4RDLQG0fV7qnp99BBtW/eq1BoI8bVkwe5LchAuPmM76GWDuroDbi', 'jacques.petit@reseau.eseo.fr', 'student', 1),
-('Sophie', 'Rousseau' , 'srousseau', '$2a$12$fA/Ihe/902MPfd8MZYoqEOXpk7GEw/oI9Gw8msi01OnM0BeCHeiDO', 'sophie.rousseau@eseo.fr', 'teaching_staff', 1),
-('Fabien', 'Chell', 'fchell', '$2a$12$SAU1bTDSVlfa5zfGwF7Ac.Ha98rqyZ3agWSb4qUh0WVMK0J2/Yfoi', 'fabien.chell@eseo.fr', 'teaching_staff', 1);
+INSERT INTO user (name, surname, login, password, email, role) VALUES
+('Jean', 'Dupont', 'jdupont', '$2a$12$beDKCRFS7AkSAzqfuVAgjemzWSbtYRMmGmg6lMmSqymZet9egfL7q', 'jean.dupont@reseau.eseo.fr', 'student'),
+('Pierre', 'Durand', 'pdurand', '$2a$12$zeVlwY3Eo6mBVM3INdB4GeXWrk8PpG4/gLs3W7.C9ZLmaLVFx9SOC', 'pierre.durand@reseau.eseo.fr', 'student'),
+('Paul', 'Martin', 'pmartin', '$2a$12$lHpX7qcs5rf4wH4CejKBKu5j3FXqPwhyHl4Iki4vd3eSYEOfC7rbm', 'paul.martin@reseau.eseo.fr', 'student'),
+('Jacques', 'Petit', 'jpetit', '$2a$12$L4RDLQG0fV7qnp99BBtW/eq1BoI8bVkwe5LchAuPmM76GWDuroDbi', 'jacques.petit@reseau.eseo.fr', 'student'),
+('Sophie', 'Rousseau' , 'srousseau', '$2a$12$fA/Ihe/902MPfd8MZYoqEOXpk7GEw/oI9Gw8msi01OnM0BeCHeiDO', 'sophie.rousseau@eseo.fr', 'teaching_staff'),
+('Fabien', 'Chell', 'fchell', '$2a$12$SAU1bTDSVlfa5zfGwF7Ac.Ha98rqyZ3agWSb4qUh0WVMK0J2/Yfoi', 'fabien.chell@eseo.fr', 'teaching_staff');
 
 INSERT INTO teaching_staff (id_user, is_infrastructure_specialist, is_development_specialist, is_modeling_specialist, is_option_leader, is_subject_validator) VALUES
 (5, 0, 1, 0, 0, 1),
