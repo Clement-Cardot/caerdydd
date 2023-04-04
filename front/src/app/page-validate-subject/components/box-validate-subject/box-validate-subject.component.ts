16546f9a0ea3389e36035bd8774eb2693fcff4c9
bonamyal <alexis.bonamy@reseau.eseo.fr>
2023-04-04 08:50:28 +0200

    create files for validate subject

---
+++ front/src/app/page-validate-subject/components/box-validate-subject/box-validate-subject.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-validate-subject',
+  templateUrl: './box-validate-subject.component.html',
+  styleUrls: ['./box-validate-subject.component.scss']
+})
+export class BoxValidateSubjectComponent {
+
+}
---
+++ front/src/app/page-validate-subject/page-validate-subject.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BoxValidateSubjectComponent } from './components/box-validate-subject/box-validate-subject.component';
+
+
+
+@NgModule({
+  declarations: [
+    BoxValidateSubjectComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PageValidateSubjectModule { }
