d36dd0867b7336533618e701d8f8e89af4b3a525
Clément CARDOT <clementcdt25@gmail.com>
2023-04-05 08:26:53 +0200

    update SQL for roles management

--- doc/populateTables.sql
+++ doc/populateTables.sql
@@ -1,16 +1,24 @@
-INSERT INTO user (name, surname, login, password, email, speciality, role) VALUES
-('Jean', 'Dupont', 'jdupont', '$2a$12$beDKCRFS7AkSAzqfuVAgjemzWSbtYRMmGmg6lMmSqymZet9egfL7q', 'jean.dupont@reseau.eseo.fr', 'LD', 'student'),
-('Pierre', 'Durand', 'pdurand', '$2a$12$zeVlwY3Eo6mBVM3INdB4GeXWrk8PpG4/gLs3W7.C9ZLmaLVFx9SOC', 'pierre.durand@reseau.eseo.fr', 'CSS', 'student'),
-('Paul', 'Martin', 'pmartin', '$2a$12$lHpX7qcs5rf4wH4CejKBKu5j3FXqPwhyHl4Iki4vd3eSYEOfC7rbm', 'paul.martin@reseau.eseo.fr', 'LD', 'student'),
-('Jacques', 'Petit', 'jpetit', '$2a$12$L4RDLQG0fV7qnp99BBtW/eq1BoI8bVkwe5LchAuPmM76GWDuroDbi', 'jacques.petit@reseau.eseo.fr', 'LD', 'student'),
-('Sophie', 'Rousseau' , 'srousseau', '$2a$12$fA/Ihe/902MPfd8MZYoqEOXpk7GEw/oI9Gw8msi01OnM0BeCHeiDO', 'sophie.rousseau@eseo.fr', NULL, 'teaching_staff'),
-('Fabien', 'Chell', 'fchell', '$2a$12$SAU1bTDSVlfa5zfGwF7Ac.Ha98rqyZ3agWSb4qUh0WVMK0J2/Yfoi', 'fabien.chell@eseo.fr', NULL, 'teaching_staff');
+INSERT INTO USER (firstname, lastname, login, password, email, speciality) VALUES
+('Jean', 'Dupont', 'jdupont', '$2a$12$beDKCRFS7AkSAzqfuVAgjemzWSbtYRMmGmg6lMmSqymZet9egfL7q', 'jean.dupont@reseau.eseo.fr', 'LD'),
+('Pierre', 'Durand', 'pdurand', '$2a$12$zeVlwY3Eo6mBVM3INdB4GeXWrk8PpG4/gLs3W7.C9ZLmaLVFx9SOC', 'pierre.durand@reseau.eseo.fr', 'CSS'),
+('Paul', 'Martin', 'pmartin', '$2a$12$lHpX7qcs5rf4wH4CejKBKu5j3FXqPwhyHl4Iki4vd3eSYEOfC7rbm', 'paul.martin@reseau.eseo.fr', 'LD'),
+('Jacques', 'Petit', 'jpetit', '$2a$12$L4RDLQG0fV7qnp99BBtW/eq1BoI8bVkwe5LchAuPmM76GWDuroDbi', 'jacques.petit@reseau.eseo.fr', 'LD'),
+('Sophie', 'Rousseau' , 'srousseau', '$2a$12$fA/Ihe/902MPfd8MZYoqEOXpk7GEw/oI9Gw8msi01OnM0BeCHeiDO', 'sophie.rousseau@eseo.fr', NULL),
+('Fabien', 'Chell', 'fchell', '$2a$12$SAU1bTDSVlfa5zfGwF7Ac.Ha98rqyZ3agWSb4qUh0WVMK0J2/Yfoi', 'fabien.chell@eseo.fr', NULL);
 
-INSERT INTO teaching_staff (id_user, is_infrastructure_specialist, is_development_specialist, is_modeling_specialist, is_option_leader, is_subject_validator) VALUES
+INSERT INTO ROLE (id_user, role) VALUES
+(1, 'STUDENT_ROLE'),
+(2, 'STUDENT_ROLE'),
+(3, 'STUDENT_ROLE'),
+(4, 'STUDENT_ROLE'),
+(5, 'TEACHING_STAFF_ROLE'),
+(6, 'TEACHING_STAFF_ROLE');
+
+INSERT INTO TEACHING_STAFF (id_user, is_infrastructure_specialist, is_development_specialist, is_modeling_specialist, is_option_leader, is_subject_validator) VALUES
 (5, 0, 1, 0, 0, 1),
 (6, 0, 0, 1, 1, 1);
 
-INSERT INTO project (name, description, is_validated) VALUES
+INSERT INTO PROJECT (name, description, is_validated) VALUES
 ('Projet 1', 'Description du projet', 0),
 ('Projet 2', 'Description du projet', 0),
 ('Projet 3', 'Description du projet', 0),
@@ -20,7 +28,7 @@ INSERT INTO project (name, description, is_validated) VALUES
 ('Projet 7', 'Description du projet', 0),
 ('Projet 8', 'Description du projet', 0);
 
-INSERT INTO team (name, id_project_dev, id_project_validation) VALUES
+INSERT INTO TEAM (name, id_project_dev, id_project_validation) VALUES
 ('Equipe 1', 1, 2),
 ('Equipe 2', 2, 1),
 ('Equipe 3', 3, 4),
