e84e569aad80b5d0fa78fe067945a7e9a249b26e
mariebdt <106916654+mariebdt@users.noreply.github.com>
2023-04-05 10:21:16 +0200

    Front sans API - affichage équipe

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login-page/components/login/login.component';
-
+import { TeamListComponent } from './teams-page/components/team-list/team-list.component';
+import { AllTeamsListComponent } from './teams-page/components/all-teams-list/all-teams-list.component';
 
 const websiteName = " - Taf"
 
 const routes: Routes = [
   { path: '',title: 'Login' + websiteName, component: LoginComponent },
+  { path: 'teams',title: 'Teams' + websiteName, component: AllTeamsListComponent },
 ];
 
 @NgModule({
