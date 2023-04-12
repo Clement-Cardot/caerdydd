ac16daa87d91ff45d1a8b9dabcfbfe26906642be
Clément CARDOT <clementcdt25@gmail.com>
2023-04-12 18:06:40 +0200

    add page name in center of head bar

--- front/src/app/sidenav/components/sidenav/sidenav.component.ts
+++ front/src/app/sidenav/components/sidenav/sidenav.component.ts
@@ -1,5 +1,7 @@
 import {MediaMatcher} from '@angular/cdk/layout';
 import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import { UserDataService } from 'src/app/core/services/user-data.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -8,20 +10,21 @@ import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 })
 export class SidenavComponent implements OnInit  {
 mobileQuery: MediaQueryList;
-
+  pageName!: String;
   fillerNav = Array.from({length: 5}, (_, i) => `Nav Item ${i + 1}`);
 
   navLink = new Array<string>;
 
   private _mobileQueryListener: () => void;
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private router: Router) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
   ngOnInit(): void {
+    this.pageName = "Accueil"
     this.navLink.push("Profil");
     this.navLink.push("Equipe");
     this.navLink.push("Notification");
