356b6a9b88d0979e195f3f906ad1c529ff4d505a
Clément CARDOT <clementcdt25@gmail.com>
2023-04-12 16:11:37 +0200

    add form to retrieve data

--- front/src/app/teams-creation-page/components/team-creation/team-creation.component.ts
+++ front/src/app/teams-creation-page/components/team-creation/team-creation.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiTeamService } from 'src/app/core/services/api-team.service';
+import { MyErrorStateMatcher } from 'src/app/login-page/components/login/login.component';
 
 @Component({
   selector: 'app-team-creation',
   templateUrl: './team-creation.component.html',
   styleUrls: ['./team-creation.component.scss']
 })
-export class TeamCreationComponent {
+export class TeamCreationComponent implements OnInit {
+  teamCreationForm!: FormGroup;
+  matcher = new MyErrorStateMatcher();
+  nbTeamsFormControl = new FormControl('', [Validators.required]);
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private apiTeamService: ApiTeamService
+  ) { }
+
+  ngOnInit(): void {
+    this.teamCreationForm = this.formBuilder.group({
+      nbTeams: this.nbTeamsFormControl
+    });
+  }
+
+  createTeams() {
+    console.log("Create teams");
+    if(this.teamCreationForm.invalid){
+      return;
+    } else {
+      this.apiTeamService.(this.teamCreationForm.value.nbTeams).subscribe(response => {
+        console.log("Response : " + response);
+      });
+    }
+  }
 
 }
--- front/src/app/teams-creation-page/teams-creation-page.module.ts
+++ front/src/app/teams-creation-page/teams-creation-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeamCreationComponent } from './components/team-creation/team-creation.component';
 import { MaterialModule } from '../material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -11,7 +12,8 @@ import { MaterialModule } from '../material.module';
   ],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule,
   ]
 })
 export class TeamsCreationPageModule { }
