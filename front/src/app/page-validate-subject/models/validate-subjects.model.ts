7983ccde1f50d83ef2cf1d79ddf918ee6f6029aa
bonamyal <alexis.bonamy@reseau.eseo.fr>
2023-04-19 11:03:23 +0200

    add description of subject + btn ok

---
+++ front/src/app/page-validate-subject/models/validate-subjects.model.ts
@@ -0,0 +1,6 @@
+export class ValidateSubject {
+    team!: string;
+    subject!: string;
+    description!: string;
+    is_validated!: boolean;
+}
--- front/src/app/page-validate-subject/services/validate-subject.service.ts
+++ front/src/app/page-validate-subject/services/validate-subject.service.ts
@@ -8,27 +8,32 @@ export class ValidateSubjectService {
     boxValidateSubject: ValidateSubject[] = [
         {
             team: "Team 1",
-            subject: "Subject 1",
-            is_validated: false
+            subject: "Emergency Management Assisant",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc sit amet ultricies lacinia, nunc nisl aliquam mauris, vitae ultricies nisl nunc eget nisl. Sed euismod, nunc sit amet ultricies lacinia, nunc nisl aliquam mauris, vitae ultricies nisl nunc eget nisl.",
+            is_validated: true
         },
         {
             team: "Team 2",
             subject: "Subject 2",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc sit amet ultricies lacinia, nunc nisl aliquam mauris, vitae ultricies nisl nunc eget nisl. Sed euismod, nunc sit amet ultricies lacinia, nunc nisl aliquam mauris, vitae ultricies nisl nunc eget nisl.",
             is_validated: false
         },
         {
             team: "Team 3",
             subject: "Subject 3",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc sit amet ultricies lacinia, nunc nisl aliquam mauris, vitae ultricies nisl nunc eget nisl. Sed euismod, nunc sit amet ultricies lacinia, nunc nisl aliquam mauris, vitae ultricies nisl nunc eget nisl.",
             is_validated: false
         },
         {
             team: "Team 4",
             subject: "Subject 4",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc sit amet ultricies lacinia, nunc nisl aliquam mauris, vitae ultricies nisl nunc eget nisl. Sed euismod, nunc sit amet ultricies lacinia, nunc nisl aliquam mauris, vitae ultricies nisl nunc eget nisl.",
             is_validated: false
         },
         {
             team: "Team 5",
             subject: "Subject 5",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc sit amet ultricies lacinia, nunc nisl aliquam mauris, vitae ultricies nisl nunc eget nisl. Sed euismod, nunc sit amet ultricies lacinia, nunc nisl aliquam mauris, vitae ultricies nisl nunc eget nisl.",
             is_validated: false
         }
     ]
