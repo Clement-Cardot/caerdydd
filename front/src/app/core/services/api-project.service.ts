efcee20b699e971a10b4c779770e4c6b77c5a512
bonamyal <alexis.bonamy@reseau.eseo.fr>
2023-04-21 11:35:12 +0200

    adapter dev (not working)

--- front/src/app/core/services/api-project.service.ts
+++ front/src/app/core/services/api-project.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from "@angular/core";
-import { Project } from "../../core/data/models/project.model";
+import { Project, ProjectDev } from "../data/models/project.model";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
     providedIn: 'root'
 })
+
+export class ApiProjectService {
+    private baseUrl: string = "http://localhost:8080/api/teams";
+
+    constructor(
+        private http : HttpClient
+    ) { }
+
+    // getAllProjects(): ProjectDev[] {
+    //     return this.http.get<any[]>(this.baseUrl)
+    //     .pipe(
+    //         map((data: any[]) => data.map((item) => this.projectAdapter.adapt(item)))
+    //     )
+    //     .pipe(
+    //         catchError(this.handleError)
+    //     );
+    // }
+}
+
 export class ProjectService {
     boxValidateSubject: Project[] = [
         {
