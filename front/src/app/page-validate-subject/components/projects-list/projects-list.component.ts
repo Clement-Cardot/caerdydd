f339b914920650e71eacec6272065df0706addef
bonamyal <alexis.bonamy@reseau.eseo.fr>
2023-04-21 11:08:52 +0200

    rename files + harmonize structure

--- front/src/app/page-validate-subject/components/projects-list/projects-list.component.ts
+++ front/src/app/page-validate-subject/components/projects-list/projects-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../../../core/data/models/project.model';
-import { ProjectService } from '../../../core/services/api-project.service';
+import { ApiProjectService } from '../../../core/services/api-project.service';
 
 @Component({
   selector: 'app-view-validate-subjects',
@@ -9,7 +9,7 @@ import { ProjectService } from '../../../core/services/api-project.service';
 })
 export class ProjectListComponent implements OnInit {
   projectList!: Project[];
-  constructor(private projectService: ProjectService) {}
+  constructor(private projectService: ApiProjectService) {}
 
   ngOnInit() {
     this.projectList = this.projectService.getAllValidateSubject();
