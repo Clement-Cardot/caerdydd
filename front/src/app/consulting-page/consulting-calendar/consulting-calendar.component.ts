5f355d991511281225d90c264a9c32251ca70ca7
cardotcl <clement.cardot@reseau.eseo.fr>
2023-04-22 00:43:30 +0200

    add consulting component

---
+++ front/src/app/consulting-page/consulting-calendar/consulting-calendar.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CalendarEvent } from 'angular-calendar';
+import { ApiConsultingService } from 'src/app/core/services/api-consulting.service';
+
+@Component({
+  selector: 'app-consulting-calendar',
+  templateUrl: './consulting-calendar.component.html',
+  styleUrls: ['./consulting-calendar.component.scss']
+})
+export class ConsultingCalendarComponent implements OnInit {
+  viewDate: Date = new Date();
+  events: CalendarEvent[] = [];
+
+  file!: File;
+
+  constructor(private apiConsultingService: ApiConsultingService) { }
+
+  ngOnInit() {
+  
+  }
+
+  selectFile(event: any) {
+    this.file = event.target.files.item(0);
+  }
+
+  upload() {
+    this.apiConsultingService.upload(this.file).subscribe(data => {
+      this.events = data
+    });
+  }
+
+}
