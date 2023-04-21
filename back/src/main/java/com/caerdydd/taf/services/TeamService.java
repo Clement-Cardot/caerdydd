3ca639dfbca590497add9db1f7e7f4295753ca46
Clément CARDOT <clementcdt25@gmail.com>
2023-04-21 12:30:40 +0200

    improve use of external functions

--- back/src/main/java/com/caerdydd/taf/services/TeamService.java
+++ back/src/main/java/com/caerdydd/taf/services/TeamService.java
@@ -131,26 +131,13 @@ public class TeamService {
         teamServiceRules.checkSpecialityRatio(team);
 
         // Check if the team is not full
-        if(team.getTeamMembers().size() == 6){
+        if(Boolean.TRUE.equals(isTeamFull(team))){
             logger.warn("ILLEGAL API USE : Team {} is full", idTeam);
             throw new CustomRuntimeException(CustomRuntimeException.TEAM_IS_FULL);
         }
 
         // check if the speciality ratio is respected (2CSS/4LD)
-        if(team.getTeamMembers().stream().filter(teamMember -> teamMember.getUser().getSpeciality().equals("CSS")).count() == 2 
-                && (securityConfig.getCurrentUser().getSpeciality().equals("CSS"))){
-
-                logger.warn("ILLEGAL API USE : Team {} already has 2 CSS", idTeam);
-                throw new CustomRuntimeException(CustomRuntimeException.TEAM_ALREADY_HAS_2_CSS);
-            
-        }
-        if(team.getTeamMembers().stream().filter(teamMember -> teamMember.getUser().getSpeciality().equals("LD")).count() == 2 
-                && (securityConfig.getCurrentUser().getSpeciality().equals("LD"))){
-                    
-                logger.warn("ILLEGAL API USE : Team {} already has 2 LD", idTeam);
-                throw new CustomRuntimeException(CustomRuntimeException.TEAM_ALREADY_HAS_2_LD);
-            
-        }
+        checkSpecialityRatio(team);
 
         // If everythings OK : create the user role "team_member" and create a new team member entity
         logger.info("Create role of User {} : team_member", idUser);
@@ -185,4 +172,20 @@ public class TeamService {
         return response;
     }
 
+    public Boolean isTeamFull(TeamDTO team) {
+        return team.getTeamMembers().size() == 6;
+    }
+
+    // check if the speciality ratio is respected (2CSS/4LD)
+    public Boolean checkSpecialityRatio(TeamDTO team) throws CustomRuntimeException{
+        if(team.getTeamMembers().stream().filter(teamMember -> teamMember.getUser().getSpeciality().equals("CSS")).count() == 2 
+                && (securityConfig.getCurrentUser().getSpeciality().equals("CSS"))){
+                throw new CustomRuntimeException(CustomRuntimeException.TEAM_ALREADY_HAS_2_CSS);
+        }
+        if(team.getTeamMembers().stream().filter(teamMember -> teamMember.getUser().getSpeciality().equals("LD")).count() == 2 
+                && (securityConfig.getCurrentUser().getSpeciality().equals("LD"))){
+                throw new CustomRuntimeException(CustomRuntimeException.TEAM_ALREADY_HAS_2_LD);
+        }
+        return true;
+    }
 }
