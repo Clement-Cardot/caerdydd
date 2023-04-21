24dcac7cf35ac3bff681a4c249bce5f0ea2bdc3f
cardotcl <clement.cardot@reseau.eseo.fr>
2023-04-21 16:01:31 +0200

    move reusable code about rules to ServiceRules

--- back/src/main/java/com/caerdydd/taf/services/TeamService.java
+++ back/src/main/java/com/caerdydd/taf/services/TeamService.java
@@ -130,15 +130,6 @@ public class TeamService {
         // check if the speciality ratio is respected (2CSS/4LD)
         teamServiceRules.checkSpecialityRatio(team);
 
-        // Check if the team is not full
-        if(Boolean.TRUE.equals(isTeamFull(team))){
-            logger.warn("ILLEGAL API USE : Team {} is full", idTeam);
-            throw new CustomRuntimeException(CustomRuntimeException.TEAM_IS_FULL);
-        }
-
-        // check if the speciality ratio is respected (2CSS/4LD)
-        checkSpecialityRatio(team);
-
         // If everythings OK : create the user role "team_member" and create a new team member entity
         logger.info("Create role of User {} : team_member", idUser);
         RoleDTO newRole = new RoleDTO();
@@ -172,20 +163,4 @@ public class TeamService {
         return response;
     }
 
-    public Boolean isTeamFull(TeamDTO team) {
-        return team.getTeamMembers().size() == 6;
-    }
-
-    // check if the speciality ratio is respected (2CSS/4LD)
-    public Boolean checkSpecialityRatio(TeamDTO team) throws CustomRuntimeException{
-        if(team.getTeamMembers().stream().filter(teamMember -> teamMember.getUser().getSpeciality().equals("CSS")).count() == 2 
-                && (securityConfig.getCurrentUser().getSpeciality().equals("CSS"))){
-                throw new CustomRuntimeException(CustomRuntimeException.TEAM_ALREADY_HAS_2_CSS);
-        }
-        if(team.getTeamMembers().stream().filter(teamMember -> teamMember.getUser().getSpeciality().equals("LD")).count() == 2 
-                && (securityConfig.getCurrentUser().getSpeciality().equals("LD"))){
-                throw new CustomRuntimeException(CustomRuntimeException.TEAM_ALREADY_HAS_2_LD);
-        }
-        return true;
-    }
 }
