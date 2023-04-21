0159bcdabd9545e3073131246e8ea3621d40f0fb
bonamyal <alexis.bonamy@reseau.eseo.fr>
2023-04-21 09:05:50 +0200

    get all projects in api

--- back/src/main/java/com/caerdydd/taf/controllers/ProjectController.java
+++ back/src/main/java/com/caerdydd/taf/controllers/ProjectController.java
@@ -4,14 +4,20 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import com.caerdydd.taf.exceptions.ResourceNotFoundException;
 import com.caerdydd.taf.models.dto.ProjectDTO;
+import com.caerdydd.taf.security.CustomRuntimeException;
 import com.caerdydd.taf.services.ProjectService;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/teams/{teamId}/projects")
+@RequestMapping("/api/projects")
 public class ProjectController {
 
     private static final Logger logger = LogManager.getLogger(ProjectController.class);
@@ -32,5 +38,20 @@ public class ProjectController {
         ProjectDTO updatedProjectDTO = projectService.updateProject(teamId, userId, projectDTO);
 
         return new ResponseEntity<>(updatedProjectDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<ProjectDTO>> getAllProjects() {
+        logger.info("Process request : Get all projects");
+        try {
+            List<ProjectDTO> projects = projectService.listAllProjects();
+            return new ResponseEntity<>(projects, HttpStatus.OK);
+        } catch (ResourceNotFoundException e) {
+            if (e.getMessage().equals(CustomRuntimeException.SERVICE_ERROR)) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            logger.error("Unexpected Exception : {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.I_AM_A_TEAPOT);
+        }
     }
 }
--- back/src/main/java/com/caerdydd/taf/models/entities/ProjectEntity.java
+++ back/src/main/java/com/caerdydd/taf/models/entities/ProjectEntity.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
@@ -24,19 +23,6 @@ public class ProjectEntity {
     private String description;
     private Boolean isValidated;
 
-    private Integer idJury;
-
-    @OneToOne(mappedBy = "projectDev")
-    private TeamEntity teamDev;
-
-    @OneToOne(mappedBy = "projectValidation")
-    private TeamEntity teamValidation;
-
-//    @OneToOne
-//    @JoinColumn(name = "id_jury")
-//    private JuryEntity jury;
-
-
     public ProjectEntity() {
     }
 
@@ -45,5 +31,10 @@ public class ProjectEntity {
         this.description=description;
     }
 
-
+    public ProjectEntity(Integer idProject, String name, String description, Boolean isValidated) {
+        this.idProject=idProject;
+        this.name=name;
+        this.description=description;
+        this.isValidated=isValidated;
+    }
 }
