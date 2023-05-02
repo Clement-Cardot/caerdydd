b25941ea0aa3cdddc955471a46bcc8b2a1314ccd
baffouau <augustin.baffou@reseau.eseo.fr>
2023-05-02 17:46:51 +0200

    Add the creation of a jury to the API

---
+++ back/src/main/java/com/caerdydd/taf/controllers/JuryService.java
@@ -0,0 +1,11 @@
+package com.caerdydd.taf.controllers;
+
+import com.caerdydd.taf.models.dto.JuryDTO;
+
+public class JuryService {
+
+    public JuryDTO addJuryMembers(Integer juryLD, Integer juryCSS) {
+        return null;
+    }
+
+}
---
+++ back/src/main/java/com/caerdydd/taf/repositories/JuryRepository.java
@@ -0,0 +1,8 @@
+package com.caerdydd.taf.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.caerdydd.taf.models.entities.JuryEntity;
+
+public interface JuryRepository extends JpaRepository<JuryEntity, Integer>{
+}
