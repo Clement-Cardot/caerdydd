c267c69ce428e5d1a8c38c94a6ca89960da6d7e0
bonamyal <alexis.bonamy@reseau.eseo.fr>
2023-05-03 11:38:26 +0200

    display team name

--- front/src/app/page-validate-subject/components/projects-list/projects-list.component.ts
+++ front/src/app/page-validate-subject/components/projects-list/projects-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../../../core/data/models/project.model';
 import { ApiProjectService } from '../../../core/services/api-project.service';
+import { ApiTeamService } from 'src/app/core/services/api-team.service';
 
 @Component({
   selector: 'app-project-list',
@@ -12,16 +13,26 @@ export class ProjectListComponent implements OnInit {
 
   refresh: any;
 
-  constructor(private projectService: ApiProjectService) {}
+  constructor(private projectService: ApiProjectService, private teamService: ApiTeamService) {}
 
   ngOnInit(): void {
     this.getAllData();
   }
 
   getAllData(){
+    // recuperer les projets
     this.projectService.getAllSubjects()
         .subscribe(data => {
             this.projectList = data;
+          }
+        );
+    // recuperer les noms des equipes
+    this.teamService.getAllTeams()
+        .subscribe(data => {
+            this.projectList.forEach(project => {
+              project.teamName = data.find(team => team.projectDev.idProject == project.idProject)?.name;
+            }
+          );
           }
         );
   }
