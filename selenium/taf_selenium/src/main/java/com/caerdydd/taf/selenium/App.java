4fc2f72946b53c19c46801a8451f5e637901098b
mariebdt <106916654+mariebdt@users.noreply.github.com>
2023-05-09 22:05:09 +0200

    Implementation Selenium tests

---
+++ selenium/taf_selenium/src/main/java/com/caerdydd/taf/selenium/App.java
@@ -0,0 +1,13 @@
+package com.caerdydd.taf.selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
