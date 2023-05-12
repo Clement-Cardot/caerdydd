b2a5478d5970f613890ac837e264a2c2295bf6f2
baffouau <augustin.baffou@reseau.eseo.fr>
2023-05-12 10:01:56 +0200

    Add teachingStaff and front done

---
+++ back/src/main/java/com/caerdydd/taf/services/TeachingStaffService.java
@@ -0,0 +1,38 @@
+package com.caerdydd.taf.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.transaction.Transactional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caerdydd.taf.models.dto.TeachingStaffDTO;
+import com.caerdydd.taf.repositories.TeachingStaffRepository;
+import com.caerdydd.taf.security.CustomRuntimeException;
+
+@Service
+@Transactional
+public class TeachingStaffService {
+    private static final Logger logger = LogManager.getLogger(TeachingStaffService.class);
+
+    @Autowired
+    private TeachingStaffRepository teachingStaffRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    public List<TeachingStaffDTO> listAllTeachingStaff() throws CustomRuntimeException {
+        try {
+            return teachingStaffRepository.findAll().stream()
+            .map(teachingStaff -> modelMapper.map(teachingStaff, TeachingStaffDTO.class))
+            .collect(Collectors.toList()) ;
+        } catch (Exception e) {
+            throw new CustomRuntimeException(CustomRuntimeException.SERVICE_ERROR);
+        }
+    }
+}
