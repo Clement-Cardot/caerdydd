408aff8b20c14e20973d3464267b5abb1db88139
bonamyal <alexis.bonamy@reseau.eseo.fr>
2023-05-12 09:29:44 +0200

    add planning assistant

--- sql/populateTables.sql
+++ sql/populateTables.sql
@@ -7,7 +7,8 @@ INSERT INTO user (firstname, lastname, login, password, email, speciality) VALUE
 ('Clive', 'Ferret-Canape', 'ferretcl', '$2a$12$pnCUeNqsAGMbGRofcX7BVO6iXB7h4PvrEQbUjINRrCOcmnNBGMI6u', 'clive.ferret-canape@eseo.fr', NULL),
 ('Richard', 'Woodward', 'woodwari', '$2a$12$X3GFVXF93Tw4PJMPzTn0WOu22WpJ.E1J8SFTXhNbgdh0airosZAkq', 'richard.woodward@eseo.fr', NULL),
 ('Jerome', 'Chavin', 'chavinje', '$2a$12$.5zlxyqsJGSwNjXQ/QucR.niSv1ubQBLv5EaHyHjs9MjTAUMdiQum', 'jerome.chavin@reseau.eseo.fr', NULL),
-('Francois', 'Jamet', 'jametfr', '$2a$12$YmratvygaJgmnxxIg/gXQOay6tlEUZa1XqwUmJ2wu7GWrFBb.USma', 'francois.jamet@reseau.eseo.fr', NULL);
+('Francois', 'Jamet', 'jametfr', '$2a$12$YmratvygaJgmnxxIg/gXQOay6tlEUZa1XqwUmJ2wu7GWrFBb.USma', 'francois.jamet@reseau.eseo.fr', NULL),
+('Alexandra', 'Bouvier', 'bouvieal', '$2a$12$G7pnCaJ7xxim5Vw3N/bbEOMJqvjgxyuKx1g1N0nS47cYXRgCM1j4a', 'alexandra.bouvier@reseau.eseo.fr', NULL);
 
 INSERT INTO role (id_user, role) VALUES
 (1, 'TEACHING_STAFF_ROLE'),
@@ -20,7 +21,8 @@ INSERT INTO role (id_user, role) VALUES
 (8, 'TEACHING_STAFF_ROLE'),
 (9, 'TEACHING_STAFF_ROLE'),
 (1, 'OPTION_LEADER_ROLE'),
-(8, 'OPTION_LEADER_ROLE');
+(8, 'OPTION_LEADER_ROLE'),
+(10, 'PLANNING_ROLE');
 
 INSERT INTO teaching_staff (id_user, is_infrastructure_specialist, is_development_specialist, is_modeling_specialist, is_option_leader, is_subject_validator) VALUES
 (1, 0, 1, 1, 1, 1),
