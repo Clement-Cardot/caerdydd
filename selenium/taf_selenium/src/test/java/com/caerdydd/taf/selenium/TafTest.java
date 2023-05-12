ecde29e639901fb3902c92754cc321f8b6634536
mariebdt <106916654+mariebdt@users.noreply.github.com>
2023-05-12 15:54:50 +0200

    Test Selenium with script and headless

--- selenium/taf_selenium/src/test/java/com/caerdydd/taf/selenium/TafTest.java
+++ selenium/taf_selenium/src/test/java/com/caerdydd/taf/selenium/TafTest.java
@@ -1,55 +1,93 @@
 package com.caerdydd.taf.selenium;
 
-
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-
+import java.io.FileInputStream;
+import java.sql.DriverManager;
 import java.time.Duration;
+import java.util.Properties;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.Dimension;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.apache.ibatis.jdbc.ScriptRunner;
+
 public class TafTest {
     
-    static WebDriver driver;
-    String websiteUrl = "http://localhost:4200/#/";
+    WebDriver driver;
+	String websiteUrl;
+	String mariaDBUrl;
+	String username;
+	String password;
 
-	@BeforeAll
-	public static void setupWebDriver() {
-		WebDriverManager.firefoxdriver().setup();
-		FirefoxOptions options = new FirefoxOptions();
-		TafTest.driver = new FirefoxDriver(options);
+	public void setupWebDriver() throws Exception{
+		String appConfigPath = "src/test/resources/application.properties";
+		Properties properties = new Properties();
+		properties.load(new FileInputStream(appConfigPath));
+
+		String profil = properties.getProperty("activeProfile");
+
+		switch(profil) {
+			case "dev":
+				websiteUrl="172.24.1.10:8080/taf/#";
+				mariaDBUrl="jdbc:mariadb://172.24.1.10:3306/ProjetGL";
+				username="webuser";
+				password="mNifUKDq10MPD3pP";
+
+				break;
+			case "local" :
+			case "@activatedProperties@" :
+			default:
+				websiteUrl="localhost:4200/taf/#";
+				mariaDBUrl="jdbc:mariadb://localhost:3306/ProjetGL";
+				username="root";
+				password="root";
+				break;
+		}	
+
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		this.driver = new ChromeDriver(options);
+	}
+
+	public void cleanupWebDriver() {
+		this.driver.close();
 	}
 	
-	@AfterAll
-	public static void cleanupWebDriver() {
-		TafTest.driver.close();
-	}
-	
-	@Test
-	@Order(1)
 	public void openConnexionPage() {
 		//Interactions avec Selenium
-		TafTest.driver.get(websiteUrl);
+		this.driver.get(this.websiteUrl);
 		driver.manage().window().setSize(new Dimension(1098, 875));
 		
 		//Assertions
 		Assertions.assertEquals("Login - Taf", driver.getTitle());
+	}
+
+	public void runSqlScript(String scriptName) throws SQLException, FileNotFoundException {
+		//Registering the Driver
+		DriverManager.registerDriver(new org.mariadb.jdbc.Driver());
+		//Getting the connection
+		Connection con = DriverManager.getConnection(mariaDBUrl, username, password);
+		System.out.println("Connection established......");
+		//Initialize the script runner
+		ScriptRunner sr = new ScriptRunner(con);
+		//Creating a reader object
+		Reader reader = new BufferedReader(new FileReader(scriptName));
+		//Running the script
+		sr.runScript(reader);
 	}
 	
   	public void connexion(String username, String password) {
@@ -68,6 +106,10 @@ public class TafTest {
   }
 
 	public void goToPage(String pageName) {
+		//Test if the menu bar is open
+		if(!driver.findElement(By.cssSelector(".mdc-icon-button > .mat-mdc-button-touch-target")).isDisplayed()) {
+			driver.findElement(By.cssSelector(".mdc-icon-button > .mat-mdc-button-touch-target")).click();
+		}
 		// Click on the page button
 		driver.findElement(By.cssSelector(".mdc-icon-button > .mat-mdc-button-touch-target")).click();
 		// Click on the page button
@@ -76,11 +118,16 @@ public class TafTest {
 		Assertions.assertEquals(pageName+" - Taf", driver.getTitle());
 	}
 
-	public void generateTeams(int teamPairsNumber) {
-		// Click on the "Créer des équipes" button
-		driver.findElement(By.cssSelector("#generateTeamsButton > .mdc-button__label")).click();
+	public void generateTeams(int teamPairsNumber) throws InterruptedException {
+		try { 
+			driver.findElement(By.cssSelector("#generateTeamsButton > .mdc-button__label")).click();
+		}
+		catch (Exception e) {
+
+		}
+		driver.findElement(By.cssSelector(".labelForm")).click();
 		// Enter the number of team pairs
-		driver.findElement(By.id("inputNumberTeamPairs")).sendKeys("2");
+		driver.findElement(By.id("inputNumberTeamPairs")).sendKeys(String.valueOf(teamPairsNumber));
 		// Click on the "Générer des équipes" button
 		driver.findElement(By.cssSelector("#buttonCreateTeam > .mdc-button__label")).click();
 		//Assertions
@@ -88,6 +135,7 @@ public class TafTest {
 	}
 
 	public void deconnexion() {
+		//Test if the menu bar is open
 		if(!driver.findElement(By.cssSelector(".mdc-icon-button > .mat-mdc-button-touch-target")).isDisplayed()) {
 			driver.findElement(By.cssSelector(".mdc-icon-button > .mat-mdc-button-touch-target")).click();
 		}
@@ -97,24 +145,10 @@ public class TafTest {
 		Assertions.assertEquals("Login - Taf", driver.getTitle());
 	}
 
-	public void applyInATeam(int numberTeam) {
-		driver.findElement(By.id("buttonJoinTeam")).click();
+	public void applyInATeam(int teamId) {
+		driver.findElement(By.id("buttonJoinTeam"+teamId)).click();
 		Assertions.assertTrue(driver.findElement(By.cssSelector(".mat-mdc-cell:nth-child(2)")).isDisplayed());
 		
 	}
-
-	@Test
-	@Order(2)
-	public void testEquipes() {
-		this.connexion("srousseau", "srousseau");
-		this.goToPage("Equipes");
-		this.generateTeams(2);
-		this.deconnexion();
-		this.connexion("pmartin", "pmartin");
-		this.goToPage("Equipes");
-		this.applyInATeam(2);
-		this.deconnexion();
-	}
-
 
 }
