fb643dbe23cdeb488d88235532e5060f0ec98a0f
bonamyal <alexis.bonamy@reseau.eseo.fr>
2023-05-17 07:14:28 +0000

    Update populateTables.sql

--- sql/populateTables.sql
+++ sql/populateTables.sql
@@ -1,6 +1,6 @@
 INSERT INTO user (firstname, lastname, login, password, email, speciality) VALUES
 ('Sophie', 'Rousseau' , 'rousseso', '$2a$12$t97oDbasmThFawvcaQkZduFrMmfEwZjB8QiWbEOJSwZoY/Hv/Xx7y', 'sophie.rousseau@eseo.fr', NULL),
-('Fabien', 'Chhel', 'chhelfa', ' $2a$12$RvdQtUaAefpCHgqBYeyb1uYuBOuJu4XM73ftSihwpssrxsA.jV0bu ', 'fabien.chhel@eseo.fr', NULL),
+('Fabien', 'Chhel', 'chhelfa', ' $2a$12$RvdQtUaAefpCHgqBYeyb1uYuBOuJu4XM73ftSihwpssrxsA.jV0bu', 'fabien.chhel@eseo.fr', NULL),
 ('Maissa', 'Abdallah', 'abdallma', '$2a$12$vGzIw1ycJIjk9AMwl20eo.WI7ugT3/unMoBttXwpec.g05P9NcD9C', 'maissa.abdallah@eseo.fr', NULL),
 ('Olivier', 'Camp', 'campol', '$2a$12$4WdyrZysmPn2a9njpTVh.eNoth4ACC.LzH8CgkQSpAyW7SOTlUZ46', 'olivier.camp@eseo.fr', NULL),
 ('Mickael', 'Clavreul', 'clavremi', '$2a$12$N7y2.IoA5BrvyYBdZCnhhecw3SUD8erxqdkYCFZQIlJygFXjBX8zW', 'mickael.clavreul@eseo.fr', NULL),
