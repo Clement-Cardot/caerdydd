ea9819a2e3e99eedfa2bb0bff18ce91fe24765c1
Clément CARDOT <clementcdt25@gmail.com>
2023-05-17 11:25:23 +0200

    improve view of dialog and add colors to calendar

--- front/src/app/consulting-page/consulting-calendar/consulting-calendar.component.ts
+++ front/src/app/consulting-page/consulting-calendar/consulting-calendar.component.ts
@@ -4,6 +4,8 @@ import { CalendarEvent } from 'angular-calendar';
 import { ApiConsultingService } from 'src/app/core/services/api-consulting.service';
 import { ClickedEventDialogComponent } from '../clicked-event-dialog/clicked-event-dialog.component';
 import { PlannedTimingConsulting } from 'src/app/core/data/models/planned-timing-consulting.model';
+import { UserDataService } from 'src/app/core/services/user-data.service';
+import { User } from 'src/app/core/data/models/user.model';
 
 class ClickEvent {
   event!: CalendarEvent;
@@ -15,16 +17,25 @@ class ClickEvent {
   styleUrls: ['./consulting-calendar.component.scss']
 })
 export class ConsultingCalendarComponent implements OnInit, OnDestroy {
+  
+  currentUser: User | null = null;
+  
   viewDate: Date = new Date();
   events: PlannedTimingConsulting[] = [];
   refresh: any;
-  
 
-  constructor(private apiConsultingService: ApiConsultingService, public dialog: MatDialog) { }
+  constructor(
+    private userDataService: UserDataService,
+    private apiConsultingService: ApiConsultingService, 
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit() {
-    this.loadConsultings();
-    this.refresh = setInterval(() => { this.loadConsultings() },  5000 );
+    this.userDataService.getCurrentUser().subscribe((user: User | null) => {
+      this.currentUser = user;
+      this.loadConsultings();
+      this.refresh = setInterval(() => { this.loadConsultings() },  5000 );
+    });
   }
 
   ngOnDestroy() {
@@ -35,10 +46,44 @@ export class ConsultingCalendarComponent implements OnInit, OnDestroy {
 
   loadConsultings() {
     this.apiConsultingService.getAllConsultings().subscribe(data => {
-      console.log(data);
+      //console.log(data);
       this.events = data;
+      this.filterConsultingsByRole();
     });
   }
+
+  filterConsultingsByRole() {
+    let currentUserRoles = this.currentUser?.getRoles();
+    if (currentUserRoles == null || currentUserRoles == undefined || currentUserRoles.length == 0) {
+      return;
+    }
+    else if (currentUserRoles.includes("TEAM_MEMBER_ROLE")) {
+      this.filterConsultingsTeamMember();
+
+    }
+    else if (currentUserRoles.includes("TEACHING_STAFF_ROLE")) {
+      this.filterConsultingsTeachingStaff();
+    }
+    else if (currentUserRoles.includes("PLANNING_ROLE")) {
+      this.filterConsultingsPlanning();
+    }
+  }
+
+  filterConsultingsTeamMember() {}
+
+  filterConsultingsTeachingStaff() {
+    this.events.forEach(event => {
+      let result = event.availabilities.find(availability => availability.teachingStaff.user.id == this.currentUser?.id);
+      if (result?.isAvailable) {
+        event.color = {primary: '#1e90ff', secondary: '#D1E8FF'};
+      }
+      else {
+        event.color = {primary: '#ad2121', secondary: '#FAE3E3'};
+      }
+    });
+  }
+
+  filterConsultingsPlanning() {}
 
   clickOnEvent(clickEvent : ClickEvent) {
     this.dialog.open(ClickedEventDialogComponent, { data: { event: clickEvent.event }});
