28d020d635c54e8cc94a1b59dd28b3264f8235fe
cardotcl <clement.cardot@reseau.eseo.fr>
2023-05-19 12:33:26 +0200

    fix after rebase

--- back/src/main/java/com/caerdydd/taf/services/TeamMemberService.java
+++ back/src/main/java/com/caerdydd/taf/services/TeamMemberService.java
@@ -92,7 +92,7 @@ public class TeamMemberService {
           // Check if the current user is a jury member 
           userServiceRules.checkCurrentUserRole("OPTION_LEADER_ROLE");
 
-        // Check if the value of the bonus is correct.
+        // Check if the value of the Individual Mark is correct.
         teamMemberServiceRules.checkTeamMemberIndividualMark(individualMark);
 
 
