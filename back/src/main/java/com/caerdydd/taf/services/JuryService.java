60a80a96a60ca3290e8fab54f0f5b3c2e6f86abd
baffouau <augustin.baffou@reseau.eseo.fr>
2023-05-25 23:13:01 +0200

    Add a check of the function addJury

--- back/src/main/java/com/caerdydd/taf/services/JuryService.java
+++ back/src/main/java/com/caerdydd/taf/services/JuryService.java
@@ -59,7 +59,6 @@ public class JuryService {
         // TODO CHECK SPECIALITY
 
         juryServiceRules.checkDifferentTeachingStaff(idJuryMemberDev, idJuryMemberArchi);
-        checkJuryExists(idJuryMemberDev, idJuryMemberArchi);
 
         TeachingStaffDTO juryMemberDev = teachingStaffService.getTeachingStaffById(idJuryMemberDev);
         TeachingStaffDTO juryMemberArchi = teachingStaffService.getTeachingStaffById(idJuryMemberArchi);
