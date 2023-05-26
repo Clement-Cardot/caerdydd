9680cca82b906d8c0823ee4047540f5376ff1916
TheoSaindrenan <120374499+TheoSaindrenan@users.noreply.github.com>
2023-05-26 15:17:31 +0200

    Derinière modif

--- back/src/main/java/com/caerdydd/taf/controllers/TeamController.java
+++ back/src/main/java/com/caerdydd/taf/controllers/TeamController.java
@@ -278,7 +278,11 @@ public class TeamController {
             if (e.getMessage().equals(CustomRuntimeException.SERVICE_ERROR)) {
                 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
-            System.out.println(e);
+            if (e.getMessage().equals(CustomRuntimeException.USER_IS_NOT_A_JURY_MEMBER)) {
+                logger.warn(e.getMessage());
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+            logger.error(UNEXPECTED_EXCEPTION, e.getMessage());
             return new ResponseEntity<>(HttpStatus.I_AM_A_TEAPOT);
         }
     }
@@ -295,7 +299,11 @@ public class TeamController {
             if (e.getMessage().equals(CustomRuntimeException.SERVICE_ERROR)) {
                 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
-            System.out.println(e);
+            if (e.getMessage().equals(CustomRuntimeException.USER_IS_NOT_A_JURY_MEMBER)) {
+                logger.warn(e.getMessage());
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+            logger.error(UNEXPECTED_EXCEPTION, e.getMessage());
             return new ResponseEntity<>(HttpStatus.I_AM_A_TEAPOT);
         }
     }
