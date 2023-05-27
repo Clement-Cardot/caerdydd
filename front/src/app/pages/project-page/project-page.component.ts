7bb1b6de44759475da2ee7959e75de83f4b089da
moermani <nicolas.moerman@reseau.eseo.fr>
2023-05-28 01:57:32 +0200

    Ajout bouton retour aux équipe + téléchargement fichier équipe

--- front/src/app/pages/project-page/project-page.component.ts
+++ front/src/app/pages/project-page/project-page.component.ts
@@ -1,7 +1,9 @@
 import { Component } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { ActivatedRoute } from "@angular/router";
 import { Team } from "src/app/core/data/models/team.model";
 import { ApiTeamService } from "src/app/core/services/api-team.service";
+import { ApiUploadFileService } from "src/app/core/services/api-upload-file.service";
 
 @Component({
     selector: 'app-project-page',
@@ -13,7 +15,7 @@ import { ApiTeamService } from "src/app/core/services/api-team.service";
     id!: string;
     team!: Team;
 
-    constructor(private route: ActivatedRoute, private apiTeamService: ApiTeamService) {  }
+    constructor(private route: ActivatedRoute, private apiTeamService: ApiTeamService, private uploadFileService: ApiUploadFileService, private _snackBar: MatSnackBar) {  }
 
     ngOnInit(): void {
       this.id = this.route.snapshot.params['id'];
@@ -21,4 +23,41 @@ import { ApiTeamService } from "src/app/core/services/api-team.service";
         this.team = data;
       });
     }
+
+    getFile(file: string) {
+      if (this.team) {
+        this.uploadFileService.download(this.team?.idTeam, file).subscribe(response=>
+        {
+          let fileName = response.headers.get('content-disposition').split(';')[2].split('=')[1];
+          fileName = fileName.slice(1, fileName.length - 1);
+          let blob : Blob = response.body as Blob;
+          let a = document.createElement('a');
+          a.download = fileName;
+          a.href = window.URL.createObjectURL(blob);
+          a.click();
+        },
+        error => {
+          {this.showErrorDownload()} 
+        });
+      }
+    }
+
+    isThereStateScope() {
+      return (this.team?.filePathScopeStatement != null);
+    }
+  
+    isThereAnalysis() {
+      return (this.team?.filePathScopeStatementAnalysis != null);
+    }
+  
+    isThereFinalStateScope() {
+      return (this.team?.filePathFinalScopeStatement != null);
+    }
+  
+    showErrorDownload() {
+      this._snackBar.open("Une erreur est survenue lors du téléchargement", "Fermer", {
+        duration: 5000,
+      });
+    }
+  
   }
