23a03ce1f1c9030750d3971a1494921bba8f4931
moermani <nicolas.moerman@reseau.eseo.fr>
2023-05-28 03:33:27 +0200

    fix css validation project + ajout dl cahier des charges

--- front/src/app/pages/validation-project-page/validation-project-page.component.ts
+++ front/src/app/pages/validation-project-page/validation-project-page.component.ts
@@ -6,6 +6,7 @@ import { ApiTeamMemberService } from 'src/app/core/services/api-team-member.serv
 import { TeamMember } from 'src/app/core/data/models/team-member.model';
 import { Team } from 'src/app/core/data/models/team.model';
 import { User } from 'src/app/core/data/models/user.model';
+import { ApiUploadFileService } from 'src/app/core/services/api-upload-file.service';
 
 @Component({
   selector: 'app-validation-project',
@@ -15,13 +16,15 @@ import { User } from 'src/app/core/data/models/user.model';
 export class ValidationProjectPageComponent implements OnInit {
   currentUser!: User | null;
   currentTeam!: Team | null;
+  validationTeam!: Team;
   testBookLinkValidation!: string | null;
 
   constructor(
     private userDataService: UserDataService,
     private apiTeamService: ApiTeamService,
     private apiTeamMemberService: ApiTeamMemberService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private uploadFileService: ApiUploadFileService
   ) { }
 
   ngOnInit(): void {
@@ -48,12 +51,26 @@ export class ValidationProjectPageComponent implements OnInit {
         this.currentTeam = team;
         if (this.currentTeam) {
           this.getTestBookLinkValidation(this.currentTeam.idTeam);
-        }
+        };
+        this.getValidationTeam();
       },
       (error) => {
         console.error("Error getting team:", error);
       }
     );
+  }
+
+  getValidationTeam() {
+    if (this.currentTeam?.projectValidation.idProject) {
+      this.apiTeamService.getTeam(this.currentTeam?.projectValidation.idProject).subscribe(
+        (team: Team) => {
+          this.validationTeam = team;
+        },
+        (error) => {
+          console.error("Error getting validation team:", error);
+        }
+      );
+    }
   }
 
   getTestBookLinkValidation(teamId: number) {
@@ -65,5 +82,33 @@ export class ValidationProjectPageComponent implements OnInit {
         console.error("Error getting test book link validation:", error);
       }
     );
+  }
+
+  getFile(file: string) {
+    if (this.currentTeam) {
+      this.uploadFileService.download(this.currentTeam?.projectValidation.idProject, file).subscribe(response=>
+      {
+        let fileName = response.headers.get('content-disposition').split(';')[2].split('=')[1];
+        fileName = fileName.slice(1, fileName.length - 1);
+        let blob : Blob = response.body as Blob;
+        let a = document.createElement('a');
+        a.download = fileName;
+        a.href = window.URL.createObjectURL(blob);
+        a.click();
+      },
+      error => {
+        {this.showErrorDownload()} 
+      });
+    }
+  }
+
+  isThereFinalStateScope() {
+    return (this.validationTeam?.filePathFinalScopeStatement != null);
+  }
+
+  showErrorDownload() {
+    this._snackBar.open("Une erreur est survenue lors du téléchargement", "Fermer", {
+      duration: 5000,
+    });
   }
 }
