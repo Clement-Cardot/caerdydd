125810bed8d4e9dd9e479be6b6c980c094e879f6
Maxime Kermagoret <maxime.kermagoret@reseau.eseo.fr>
2023-05-31 14:11:50 +0200

    38 - back get consulting by speciality works

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -18,45 +18,129 @@ import { DefineSpecialtyComponent } from './teaching-staff-page/components/defin
 import { ValidationProjectComponent } from './validation-project/component/validation-project/validation-project.component';
 import { TeamInfoComponent } from './teams-page/components/team-info/team-info.component';
 
-const websiteName = " - Taf"
+const websiteName = ' - Taf';
 
 const routes: Routes = [
   { path: '', title: 'Login' + websiteName, component: LoginComponent },
 
   // Main Page
-  { path: 'dashboard', title: 'Tableau de bord' + websiteName, component: DashboardComponent, canActivate: [AuthGuard] },
+  {
+    path: 'dashboard',
+    title: 'Tableau de bord' + websiteName,
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
 
   // Administration page
-  /* TODO */ { path: 'administration', title: 'Administration' + websiteName, component: DashboardComponent, canActivate: [AuthGuard, RoleGuard], data: {roles: ['OPTION_LEADER_ROLE']}},
+  /* TODO */ {
+    path: 'administration',
+    title: 'Administration' + websiteName,
+    component: DashboardComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['OPTION_LEADER_ROLE'] },
+  },
 
   // Planification pages
-  { path: 'planning', title: 'Planification' + websiteName, component: PlanificationPageComponent, canActivate: [AuthGuard, RoleGuard], data: {roles: ['PLANNING_ROLE']}},
-  { path: 'consultings', title: 'Consultings' + websiteName, component: ConsultingCalendarComponent, canActivate: [AuthGuard] },
+  {
+    path: 'planning',
+    title: 'Planification' + websiteName,
+    component: PlanificationPageComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['PLANNING_ROLE'] },
+  },
+  {
+    path: 'consultings',
+    title: 'Consultings' + websiteName,
+    component: ConsultingCalendarComponent,
+    canActivate: [AuthGuard],
+  },
 
   // Notation pages
-  { path: 'marks', title: 'Notation' + websiteName, component: NotationComponent, canActivate: [AuthGuard, RoleGuard], data: {roles: ['OPTION_LEADER_ROLE', 'TEACHING_STAFF_ROLE']}},
+  {
+    path: 'marks',
+    title: 'Notation' + websiteName,
+    component: NotationComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['OPTION_LEADER_ROLE', 'TEACHING_STAFF_ROLE'] },
+  },
 
   // Subject page
-  { path: 'subjects',title: 'Sujets' + websiteName, component: ProjectListComponent, canActivate: [AuthGuard, RoleGuard], data: {roles: ['OPTION_LEADER_ROLE']}},
+  {
+    path: 'subjects',
+    title: 'Sujets' + websiteName,
+    component: ProjectListComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['OPTION_LEADER_ROLE'] },
+  },
 
   // Teams pages
-  { path: 'teams', title: 'Teams' + websiteName, component: AllTeamsListComponent, canActivate: [AuthGuard] },
-  /* TODO */ { path: 'my-team', title: 'Teams' + websiteName, component: DashboardComponent, canActivate: [AuthGuard, RoleGuard], data: {roles: ['TEAM_MEMBER_ROLE']}},
-  { path: 'file', title: 'Teams' + websiteName, component: ProjectFileComponent, canActivate: [AuthGuard] },
-  { path: 'teams/:id', title: 'Teams Info' + websiteName, component: TeamInfoComponent, canActivate: [AuthGuard]},
+  {
+    path: 'teams',
+    title: 'Teams' + websiteName,
+    component: AllTeamsListComponent,
+    canActivate: [AuthGuard],
+  },
+  /* TODO */ {
+    path: 'my-team',
+    title: 'Teams' + websiteName,
+    component: DashboardComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['TEAM_MEMBER_ROLE'] },
+  },
+  {
+    path: 'file',
+    title: 'Teams' + websiteName,
+    component: ProjectFileComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'teams/:id',
+    title: 'Teams Info' + websiteName,
+    component: TeamInfoComponent,
+    canActivate: [AuthGuard],
+  },
 
   // Projects pages
-  { path: 'dev-project', title: 'Projet de Développement' + websiteName, component: DevProjectComponent, canActivate: [AuthGuard, RoleGuard], data: {roles: ['TEAM_MEMBER_ROLE']}},
-  { path: 'validation-project', title: 'Projet de Validation' + websiteName, component: ValidationProjectComponent, canActivate: [AuthGuard, RoleGuard], data: {roles: ['TEAM_MEMBER_ROLE']}},
+  {
+    path: 'dev-project',
+    title: 'Projet de Développement' + websiteName,
+    component: DevProjectComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['TEAM_MEMBER_ROLE'] },
+  },
+  {
+    path: 'validation-project',
+    title: 'Projet de Validation' + websiteName,
+    component: ValidationProjectComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['TEAM_MEMBER_ROLE'] },
+  },
 
   // Temporaire pour tester
-  { path: 'projectD', title: 'Project-description' + websiteName, component: ProjectDescriptionComponent, canActivate: [AuthGuard] },
-  
+  {
+    path: 'projectD',
+    title: 'Project-description' + websiteName,
+    component: ProjectDescriptionComponent,
+    canActivate: [AuthGuard],
+  },
+
   { path: 'error', title: 'Erreur' + websiteName, component: ErrorComponent },
 
   //TeachingStaff Pages
-  { path: 'teachingStaffSpe', title: 'Corps Enseignant' + websiteName, component: AllTeachingStaffComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['TEACHING_STAFF_ROLE'] }},
-  { path: 'teachingStaffSpe/modifySpeciality', title: 'Modifier Specialités' + websiteName, component: DefineSpecialtyComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['TEACHING_STAFF_ROLE'] }},
+  {
+    path: 'teachingStaffSpe',
+    title: 'Corps Enseignant' + websiteName,
+    component: AllTeachingStaffComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['TEACHING_STAFF_ROLE'] },
+  },
+  {
+    path: 'teachingStaffSpe/modifySpeciality',
+    title: 'Modifier Specialités' + websiteName,
+    component: DefineSpecialtyComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['TEACHING_STAFF_ROLE'] },
+  },
 ];
 
 @NgModule({
--- sql/tablesBdd.sql
+++ sql/tablesBdd.sql
@@ -106,13 +106,13 @@ CREATE TABLE consulting (
     id_consulting INT NOT NULL AUTO_INCREMENT,
     speciality ENUM('infrastructure', 'development', 'modeling'),
     notes VARCHAR(250),
-    is_validated BOOLEAN NOT NULL,
-    is_reserved BOOLEAN NOT NULL,
     id_team INT NOT NULL,
     id_planned_timing_availability INT NOT NULL,
+    id_planned_timing_consulting INT NOT NULL,
     PRIMARY KEY(id_consulting),
     FOREIGN KEY (id_team) REFERENCES team (id_team),
-    FOREIGN KEY (id_planned_timing_availability) REFERENCES planned_timing_availability (id_planned_timing_availability)
+    FOREIGN KEY (id_planned_timing_availability) REFERENCES planned_timing_availability (id_planned_timing_availability),
+    FOREIGN KEY (id_planned_timing_consulting) REFERENCES planned_timing_consulting (id_planned_timing_consulting)
 );
 
 CREATE TABLE team_member (
